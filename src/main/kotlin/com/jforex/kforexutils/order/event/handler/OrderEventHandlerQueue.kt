ee95bcb7153fa6241972672c374bc668a2560b5c
juxeii <juxeii@gmx.de>
2018-11-10 11:58:09 +0100

    Prototype functions for vent handler state

--- src/main/kotlin/com/jforex/kforexutils/order/event/handler/OrderEventHandlerQueue.kt
+++ src/main/kotlin/com/jforex/kforexutils/order/event/handler/OrderEventHandlerQueue.kt
@@ -1,10 +1,51 @@
 package com.jforex.kforexutils.order.event.handler
 
+import com.dukascopy.api.IOrder
 import com.jforex.kforexutils.order.event.OrderEvent
 import com.jforex.kforexutils.order.event.handler.data.OrderEventHandlerData
 import com.jforex.kforexutils.order.event.subscribeToOrderEvents
 import io.reactivex.Observable
+import java.util.*
 import java.util.concurrent.ConcurrentLinkedQueue
+
+typealias MappedHandlerQueue = Map<IOrder, Queue<OrderEventHandlerData>>
+
+internal fun addHandler(
+    order: IOrder,
+    handlerData: OrderEventHandlerData,
+    orderEvents: Observable<OrderEvent>,
+    handlerQueues: MappedHandlerQueue
+)
+{
+    val queue = handlerQueues.getValue(order)
+    if (queue.isEmpty())
+    {
+        subscribeHandler(order, handlerData, orderEvents, handlerQueues)
+    }
+    queue.add(handlerData)
+}
+
+private fun subscribeHandler(
+    order: IOrder,
+    handlerData: OrderEventHandlerData,
+    orderEvents: Observable<OrderEvent>,
+    handlerQueues: MappedHandlerQueue
+)
+{
+    val nextCall = { subscribeNextHandler(order, orderEvents, handlerQueues) }
+    subscribeToOrderEvents(handlerData, nextCall).run(orderEvents)
+}
+
+private fun subscribeNextHandler(
+    order: IOrder,
+    orderEvents: Observable<OrderEvent>,
+    handlerQueues: MappedHandlerQueue
+)
+{
+    val queue = handlerQueues.getValue(order)
+    queue.poll()
+    if (!queue.isEmpty()) subscribeHandler(order, queue.peek(), orderEvents, handlerQueues)
+}
 
 class OrderEventHandlerQueue(private val orderEvents: Observable<OrderEvent>)
 {
