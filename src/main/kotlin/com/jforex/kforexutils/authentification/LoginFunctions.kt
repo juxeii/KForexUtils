52b67347b62693717aabc76ae2c5b806089688f3
juxeii <juxeii@gmx.de>
2018-11-24 11:27:32 +0100

    Using an applicative for LoginType creation.

--- src/main/kotlin/com/jforex/kforexutils/authentification/LoginFunctions.kt
+++ src/main/kotlin/com/jforex/kforexutils/authentification/LoginFunctions.kt
@@ -1,25 +1,33 @@
 package com.jforex.kforexutils.authentification
 
 import arrow.core.None
-import arrow.core.Option
+import arrow.core.Some
 import arrow.data.ReaderApi
+import arrow.data.fix
 import arrow.data.map
+import arrow.instances.applicative
 import com.dukascopy.api.system.IClient
 import com.jforex.kforexutils.client.pinProvider
 import com.jforex.kforexutils.client.platformSettings
 
-internal fun createLoginData(type: LoginType) = ReaderApi
+internal fun ibne(type: LoginType) = ReaderApi
+    .applicative<IClient>()
+    .tupled(jNLPAddressForLoginType(type), pinForLoginType(type))
+    .fix()
+    .map { LoginData(jnlpAddress = it.a, maybePin = it.b) }
+
+internal fun jNLPAddressForLoginType(type: LoginType) = ReaderApi
     .ask<IClient>()
     .map {
         with(it.platformSettings) {
-            if (type == LoginType.DEMO)
-                LoginData(
-                    jnlpAddress = demoConnectURL(),
-                    maybePin = None
-                )
-            else LoginData(
-                jnlpAddress = liveConnectURL(),
-                maybePin = Option.just(it.pinProvider.pin)
-            )
+            if (type == LoginType.DEMO) demoConnectURL()
+            else liveConnectURL()
         }
+    }
+
+internal fun pinForLoginType(type: LoginType) = ReaderApi
+    .ask<IClient>()
+    .map {
+        if (type == LoginType.DEMO) None
+        else Some(it.pinProvider.pin)
     }
