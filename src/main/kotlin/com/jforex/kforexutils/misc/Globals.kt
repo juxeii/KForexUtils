45c33c317d01370020371ae4ae90abcd9649a410
juxeii <juxeii@gmx.de>
2018-12-26 14:07:00 +0100

    Added rounding for amounts.

--- src/main/kotlin/com/jforex/kforexutils/misc/Globals.kt
+++ src/main/kotlin/com/jforex/kforexutils/misc/Globals.kt
@@ -1,6 +1,7 @@
 package com.jforex.kforexutils.misc
 
 import com.jforex.kforexutils.order.task.OrderCallHandlers
+import java.math.BigDecimal
 
 @DslMarker
 annotation class OrderDsl
@@ -10,3 +11,8 @@ internal val emptyOrderConsumer: OrderConsumer = {}
 internal val emptyErrorConsumer: ErrorConsumer = { }
 internal val emptyOrderEventConsumer: OrderEventConsumer = {}
 internal val emptyCallHandlers = OrderCallHandlers()
+
+fun Double.toAmount() =
+    BigDecimal(this)
+        .setScale(2, BigDecimal.ROUND_HALF_UP)
+        .toDouble()
